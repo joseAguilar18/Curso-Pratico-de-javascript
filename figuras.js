d363469a0a90eb9435889573fac6b66538c28d15
Jose Aguilar <josefrainal20000@outlook.com>
2021-09-27 23:43:05 -0600

    Se definiron las constantes de los valores de cada figura, asi como sus respectivas formulas atraves de variables.

--- figuras.js
+++ figuras.js
@@ -1,1 +1,59 @@
-console.log("Hola")
+//codigo del cuadrado
+console.group("Cuadrados");
+const ladoCuadrado = 5;
+console.log("Los lados del cuadrado miden: "+ ladoCuadrado +"cm");
+
+const perimetroCuadrado = ladoCuadrado * 4;
+
+console.log("El perimetro del cuadrado es: "+ perimetroCuadrado+"cm");
+
+const areaCuadrado = ladoCuadrado * ladoCuadrado;
+
+console.log("El area del cuadrado es: "+ areaCuadrado+"cm^2");
+console.groupEnd();
+//codigo del triangulo
+
+console.group("Triangulos");
+const ladoTriangulo1 = 6;
+const ladoTriangulo2 = 6;
+const baseTriangulo = 4;
+
+console.log("Los lados del triangulo miden: "+ 
+ladoTriangulo1+"cm , "
++ladoTriangulo2+"cm, "
++baseTriangulo+"cm");
+
+const alturaTriangulo = 5.5;
+console.log("El altura del triangulo es : "+ alturaTriangulo+"cm");
+
+const perimetroTriangulo = ladoTriangulo1 + ladoTriangulo2 + baseTriangulo;
+console.log("El perimetro del triangulo es: "+ perimetroTriangulo + "cm")
+
+const areaTriangulo = (baseTriangulo * alturaTriangulo)/2;
+console.log("El area del triangulo es: " + areaTriangulo + "cm^2")
+
+console.groupEnd();
+
+//codigo del circulo
+
+console.group("Circulo");
+
+//Radio
+const radioCirculo = 4
+console.log("El radio del circulo es: "+ radioCirculo +"cm");
+//diametro
+const diametroCirculo = radioCirculo * 2;
+console.log("El diametro del circulo es: "+ diametroCirculo +"cm");
+//PI
+const PI = Math.PI;
+console.log("El valor de PI es: "+ PI);
+//circunferencia
+const perimetroCirculo = diametroCirculo * PI;
+console.log("La circunferencia del circulo es: "+ perimetroCirculo +"cm");
+//Area
+const areaCirculo = Math.pow(radioCirculo,2) * PI;
+console.log("El area del circulo es: "+ areaCirculo +"cm");
+
+console.groupEnd();
+
+
